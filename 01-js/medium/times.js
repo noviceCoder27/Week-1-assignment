5e1681419052530fe8a1618b9128ff8251ae2677
mdGoswami <u19cse1001@cit.ac.in>
2023-06-13 14:07:10 +0530

    chore: calculate time reqd for heavy operations

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,5 +8,12 @@ Hint - use Date class exposed in JS
 */
 
 function calculateTime(n) {
-    return 0.01;
+    const time = new Date().getSeconds();
+    let sum = 0;
+    for(let i = 1; i <=n; i++) {
+        sum += i;
+    }
+
+    const currentTime = new Date().getSeconds();
+    return currentTime - time;
 }

98570199be76d3c7f741e73ccab2fe8f6b9751ec
mdGoswami <u19cse1001@cit.ac.in>
2023-06-13 16:43:28 +0530

    chore: complete todo-list exercise

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,6 +12,39 @@
 */
 
 class Todo {
+  constructor() {
+    this.todos = [] 
+  }
+
+  getAll() {
+    return this.todos;
+  }
+
+  get(index) {
+    if(index >= this.todos.length) {
+      return null;
+    }
+    return this.todos[index];
+  }
+
+  add(todo) {
+    this.todos.push(todo);
+  }
+  remove(index) {
+    this.todos.splice(index,1);
+  }
+
+  update(index,updatedTodo) {
+    if(index < this.todos.length) {
+      this.todos.splice(index,0,updatedTodo);
+      this.todos.splice(index+1,1);
+    }
+    
+  }
+
+  clear() {
+    this.todos = [];
+  }
 
 }
 
